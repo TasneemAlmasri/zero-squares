b8b988663e06664cc6c6df6ab8b419ebff5f480c
unknown <tasneemmasri4@gmail.com>
2024-10-29 23:53:53 +0300

    initial commit

---
+++ src/Point.java
@@ -0,0 +1,16 @@
+public class Point {
+    public int row;
+    public int col;
+
+    public Point(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    
+            
+
+    // @Override
+    // public String toString() {
+    //     return "(" + row + ", " + col + ")";
+    // }
+}
